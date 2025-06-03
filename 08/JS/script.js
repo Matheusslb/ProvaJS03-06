a7d647d52d52f820e63ad8f86eed8522b9855c04
SaCasseta <scasseta@gmail.com>
2025-06-03 16:42:04 -0300

    in progress

---
+++ 08/JS/script.js
@@ -0,0 +1,88 @@
+function configurar() {
+  let inserirBtn = document.getElementById('inserirBtn');
+  let nomeInput = document.getElementById('nome');
+
+  //prettier-ignore
+ if ((inserirBtn instanceof HTMLButtonElement) && (nomeInput instanceof HTMLInputElement)
+  ) {
+    inserirBtn.addEventListener('click', () => {
+      let nome = nomeInput.value;
+      array.push(validarNome(nome));
+    exibirConteudo();
+    });
+  }
+}
+
+function validarNome(nome) {
+  try {
+    let regex = /[^a-zA-ZÀ-ÿ \s]/g;
+
+    if (regex.test(nome)) {
+      throw new Error(
+        'O nome deve conter apenas letras e espaços entre o nome e sobrenome.'
+      );
+    }
+    // Verifica se o nome tem pelo menos duas palavras
+    if (nome.trim().split(/\s+/).length < 2) {
+      throw new Error(
+        'O nome deve ter pelo menos duas palavras (nome e sobrenome).'
+      );
+    }
+
+    exibirMensagem(exibirConteudo);
+  } catch (erro) {
+    exibirMensagem(erro.message);
+  }
+}
+ 
+function exibirMensagem(mensagem) {
+  let saida = document.getElementById('saida');
+
+  if (saida instanceof HTMLParagraphElement) {
+    saida.textContent = mensagem;
+  }
+}
+
+
+const array = [];
+
+function exibirConteudo() {
+  let saida = document.getElementById('saida');
+
+  if (saida instanceof HTMLParagraphElement) {
+    saida.textContent = array.join(', ');
+  }
+}
+
+
+
+
+function exibirSorteio() {
+  //onde item nesse contexto é uma ul
+  let item = document.getElementById('nome');
+
+  if (item instanceof HTMLUListElement) {
+    while (item.firstChild) {
+      item.removeChild(item.firstChild);
+    }
+
+    array.forEach((valorAtualArray) => {
+      // os itens 'li' da lista são criados de forma dinâmica
+      let li = document.createElement('li');
+      li.textContent = valorAtualArray;
+      item.appendChild(li);
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', configurar);
